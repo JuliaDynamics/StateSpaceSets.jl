c60a0c31377c5eacbfe897e152bbb2df7a78fecc
Datseris <datseris.george@gmail.com>
2022-11-10 10:30:01 +0100

    add documentation from DynamicalSystems.jl (to be improved)

---
+++ docs/make.jl
@@ -0,0 +1,59 @@
+cd(@__DIR__)
+using Pkg
+CI = get(ENV, "CI", nothing) == "true" || get(ENV, "GITHUB_TOKEN", nothing) !== nothing
+using StateSpaceSets
+using Documenter
+using DocumenterTools: Themes
+
+# %% JuliaDynamics theme
+# It includes themeing for the HTML build
+# and themeing for the Makie plotting
+
+using DocumenterTools: Themes
+for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
+    filepath = joinpath(@__DIR__, file)
+    if !isfile(filepath)
+        download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
+    end
+end
+# create the themes
+for w in ("light", "dark")
+    header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
+    theme = read(joinpath(@__DIR__, "juliadynamics-$(w)defs.scss"), String)
+    write(joinpath(@__DIR__, "juliadynamics-$(w).scss"), header*"\n"*theme)
+end
+# compile the themes
+Themes.compile(joinpath(@__DIR__, "juliadynamics-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
+Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
+
+# %% Build docs
+ENV["JULIA_DEBUG"] = "Documenter"
+
+STATESPACESETS_PAGES = [
+    "index.md",
+]
+include("style.jl")
+
+makedocs(
+    modules = [StateSpaceSets],
+    format = Documenter.HTML(
+        prettyurls = CI,
+        assets = [
+            asset("https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap", class=:css),
+        ],
+        collapselevel = 3,
+        ),
+    sitename = "StateSpaceSets.jl",
+    authors = "George Datseris",
+    pages = STATESPACESETS_PAGES,
+    doctest = false,
+    draft = false,
+)
+
+if CI
+    deploydocs(
+        repo = "github.com/JuliaDynamics/StateSpaceSets.jl.git",
+        target = "build",
+        push_preview = true
+    )
+end
--- src/StateSpaceSets.jl
+++ src/StateSpaceSets.jl
@@ -1,3 +1,13 @@
+"""
+A Julia package that provides an interface for datasets, or more specifically, state space
+sets. These are collections of points of fixed, and known by type, size (called dimension).
+The main export of the package is the [`Dataset`](@ref) type, and all its surrounding
+convenience functions. It is used in several projects in the JuliaDynamics organization,
+such as [DynamicalSystems.jl](https://juliadynamics.github.io/DynamicalSystems.jl/dev/)
+or [CausalityTools.jl](https://juliadynamics.github.io/CausalityTools.jl/dev/).
+
+To install it, run `import Pkg; Pkg.add("StateSpaceSets")`.
+"""
 module StateSpaceSets
 
 include("dataset.jl")
