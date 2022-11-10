dae5b485a067b56697ccd96b3ff0bdb0168b17fc
Datseris <datseris.george@gmail.com>
2022-11-10 08:54:18 +0100

    port source codehere from DelayEmbeddings.jl

--- src/StateSpaceSets.jl
+++ src/StateSpaceSets.jl
@@ -1,5 +1,10 @@
 module StateSpaceSets
 
-greet() = print("Hello World!")
+include("dataset.jl")
+include("subdataset.jl")
+include("neighborhoods.jl")
+include("dataset_distance.jl")
+include("utils.jl")
+include("sampler.jl")
 
 end # module StateSpaceSets
---
+++ test/runtests.jl
@@ -0,0 +1,9 @@
+using Test
+
+defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => ' '))
+testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
+@testset "StateSpaceSets.jl" begin
+    testfile("dataset_tests.jl")
+    testfile("dataset_distance_tests.jl")
+    testfile("utils_tests.jl")
+end
