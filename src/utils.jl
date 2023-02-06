b03549960e96d4d8e0a91f2f51c2ea91614fa7af
Datseris <datseris.george@gmail.com>
2023-02-06 15:19:52 +0000

    correct docstring for `orthonromal`

--- src/utils.jl
+++ src/utils.jl
@@ -25,9 +25,9 @@ end
     orthonormal([T,] D, k) -> ws
 Return a matrix `ws` with `k` columns, each being
 an `D`-dimensional orthonormal vector.
-`T` is the return type and can be either `SMatrix` or `Matrix`
 
-returns either SMatrix{D, k} if D*k < 100, otherwise Dxk Matrix{Float64}
+`T` is the return type and can be either `SMatrix` or `Matrix`.
+If not given, it is `SMatrix` if `D*k < 100`, otherwise `Matrix`.
 """
 function orthonormal end
 
