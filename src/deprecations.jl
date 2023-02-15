4118b7675cbe315bf6ee355d5a12d227d886f1da
Datseris <datseris.george@gmail.com>
2023-02-15 11:28:49 +0000

    use `const` to export deprecated datraset types

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,4 +1,5 @@
 @deprecate dataset_distance set_distance
 @deprecate datasets_sets_distance setsofsets_distance
-@deprecate AbstractDataset AbstractStateSpaceSet
-@deprecate Dataset StateSpaceSet
+const AbstractDataset = AbstractStateSpaceSet
+const Dataset = StateSpaceSet
+export Dataset, AbstractDataset
