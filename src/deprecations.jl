bc67f2841a2e06eabd8f0aed8bada80523aadc07
Datseris <datseris.george@gmail.com>
2023-02-15 11:37:22 +0000

    okay in the end you need `const` for type deprecations...

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,4 +1,5 @@
 @deprecate dataset_distance set_distance
 @deprecate datasets_sets_distance setsofsets_distance
-@deprecate AbstractDataset AbstractStateSpaceSet
-@deprecate Dataset StateSpaceSet
+export AbstractDataset, Dataset
+const AbstractDataset = AbstractStateSpaceSet
+const Dataset = StateSpaceSet
