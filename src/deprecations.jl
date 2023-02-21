541c08d0aeb92f851292aa7b49fb5378edd0f231
Datseris <datseris.george@gmail.com>
2023-02-21 12:03:29 +0000

    fix typo in wrong deprecation

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,5 +1,5 @@
 @deprecate dataset_distance set_distance
-@deprecate datasets_sets_distance setsofsets_distance
+@deprecate datasets_sets_distances setsofsets_distance
 export AbstractDataset, Dataset
 const AbstractDataset = AbstractStateSpaceSet
 const Dataset = StateSpaceSet
