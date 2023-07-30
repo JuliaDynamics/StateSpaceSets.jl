162ebf9a4fb4ad0a547d51c4bd0b32544554f97a
Datseris <datseris.george@gmail.com>
2023-07-30 08:56:43 +0100

    fix tests

--- src/sampler.jl
+++ src/sampler.jl
@@ -1,6 +1,7 @@
 export statespace_sampler
 using Distributions, LinearAlgebra, Random
-# TODO: I think these functions aren't tested...?
+# TODO: Performance can be improved signficantly here by employing dedicated structs
+# that have inner vectors that are updated in-place.
 
 """
     statespace_sampler(rng = Random.GLOBAL_RNG; kwargs...) → sampler, isinside
@@ -10,7 +11,7 @@ that generates random points inside a state space region defined by the keywords
 state space point is inside that region.
 
 The regions can be:
-* **Rectangular box**, with edges `min_bounds` and `max_bounds`.
+* **Rectangular box**, with edges `min_bounds` (inclusive) and `max_bounds` (exclusive).
   The sampling of the points inside the box is decided by the keyword `method` which can
   be either `"uniform"` or `"multgauss"`.
 * **Sphere**, of `spheredims` dimensions, radius `radius` and centered on `center`.
@@ -71,7 +72,7 @@ and `isinside` that returns `true` if a given state is in the box.
 function boxregion(as, bs, rng = Random.GLOBAL_RNG)
     @assert length(as) == length(bs) > 0
     gen() = [rand(rng)*(bs[i]-as[i]) + as[i] for i in 1:length(as)]
-    isinside(x) = all(as .< x .< bs)
+    isinside(x) = all(as .≤ x .< bs)
     return gen, isinside
 end
 
@@ -79,7 +80,7 @@ end
 function boxregion(a::Real, b::Real, rng = Random.GLOBAL_RNG)
     a, b = extrema((a, b))
     gen() = rand(rng)*(b-a) + a
-    isinside = x -> a < x < b
+    isinside = x -> a ≤ x < b
     return gen, isinside
 end
 
@@ -92,10 +93,10 @@ function sphereregion(r, dim, center, rng)
     function generator()
         randn!(rng, dummy)
         dummy ./= LinearAlgebra.norm(dummy)
-        dummy .*= r
+        dummy .*= rand(rng)*r
         dummy .+= center
         return dummy
     end
-    isinside(x) = norm(x .- center) < r
+    isinside(x) = norm(x .- center) ≤ r
     return generator, isinside
 end
--- test/sampler_tests.jl
+++ test/sampler_tests.jl
@@ -1,93 +1,44 @@
+using StateSpaceSets
 using LinearAlgebra, Random, Statistics, Test
 
-println("\nTesting sampler...")
+@testset "rectangular box" begin
+    @testset "$(method)" for method in ("uniform", "multgauss")
+        rng = MersenneTwister(1234)  # Seed random number generator for reproducibility
 
-@testset "statespace_sampler" begin
+        # Define rectangular box region
+        min_bounds = [-1, -2]
+        max_bounds = [1, 2]
 
-# Test statespace_sampler with rectangular box method
-function test_statespace_sampler_rectangular_box()
-    rng = MersenneTwister(1234)  # Seed random number generator for reproducibility
+        # Generate sampler and isinside functions
+        gen, isinside = statespace_sampler(rng; min_bounds=min_bounds, max_bounds=max_bounds, method=method)
 
-    # Define rectangular box region
-    min_bounds = [-1, -2]
-    max_bounds = [1, 2]
-    method = "uniform"
-
-    # Generate sampler and isinside functions
-    gen, isinside = statespace_sampler(rng; min_bounds=min_bounds, max_bounds=max_bounds, method=method)
-
-    # Test generated points are inside the box region
-    for i in 1:250
-        x = gen()
-        @test all(min_bounds .<= x .<= max_bounds)
-        @test isinside(x)
+        # Test generated points are inside the box region
+        for i in 1:250
+            x = gen()
+            @test all(min_bounds .<= x .<= max_bounds)
+            @test isinside(x)
+        end
     end
 end
 
-# Test statespace_sampler with multivariate Gaussian box method
-function test_statespace_sampler_multivariate_gaussian_box()
-    rng = MersenneTwister(1234)  # Seed random number generator for reproducibility
+@testset "sphere" begin
+    for radius in (0.1, 4.0)
+        for spheredims in (2, 4)
 
-    # Define rectangular box region
-    min_bounds = [-1, -2]
-    max_bounds = [1, 2]
-    method = "multgauss"
+            rng = MersenneTwister(1234)  # Seed random number generator for reproducibility
 
-    # Generate sampler and isinside functions
-    gen, isinside = statespace_sampler(rng; min_bounds=min_bounds, max_bounds=max_bounds, method=method)
+            center = fill(rand(), spheredims)
 
-    # Test generated points are inside the box region
-    for i in 1:250
-        x = gen()
-        @test all(min_bounds .<= x .<= max_bounds)
-        @test isinside(x)
+            # Generate sampler and isinside functions
+            gen, isinside = statespace_sampler(rng; radius=radius, spheredims=spheredims, center=center)
+
+            # Test generated points are inside the sphere region
+            for i in 1:50
+                x = gen()
+                @test norm(x - center) ≤ radius
+                @test isinside(x)
+            end
+
+        end
     end
-end
-
-# Test statespace_sampler with sphere method
-function test_statespace_sampler_sphere_3D()
-    rng = MersenneTwister(1234)  # Seed random number generator for reproducibility
-
-    # Define sphere region
-    radius = 2
-    spheredims = 3
-    center = [1, 2, 3]
-
-    # Generate sampler and isinside functions
-    gen, isinside = statespace_sampler(rng; radius=radius, spheredims=spheredims, center=center)
-
-    # Test generated points are inside the sphere region
-    for i in 1:250
-        x = gen()
-        @test norm(x - center) <= radius
-        @test isinside(x)
-    end
-end
-
-# Test statespace_sampler with sphere method
-function test_statespace_sampler_sphere_4D()
-    rng = MersenneTwister(1234)  # Seed random number generator for reproducibility
-
-    # Define sphere region
-    radius = 2
-    spheredims = 4
-    center = [1, 2, 3, 7]
-
-    # Generate sampler and isinside functions
-    gen, isinside = statespace_sampler(rng; radius=radius, spheredims=spheredims, center=center)
-
-    # Test generated points are inside the sphere region
-    for i in 1:250
-        x = gen()
-        @test norm(x - center) <= radius
-        @test isinside(x)
-    end
-end
-
-
-# Run tests
-    test_statespace_sampler_rectangular_box()
-    test_statespace_sampler_multivariate_gaussian_box()
-    test_statespace_sampler_sphere_3D()
-    test_statespace_sampler_sphere_4D()
 end
