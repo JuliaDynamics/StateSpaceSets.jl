038542d624a53cd936c7096a804ece4cc2e21d7e
Datseris <datseris.george@gmail.com>
2024-10-07 14:59:02 +0100

    expose the keywords

--- src/set_distance.jl
+++ src/set_distance.jl
@@ -53,6 +53,9 @@ The distance is calculated with the metric given to `Hausdorff` which defaults t
 a proper metric in the space of sets of state space sets.
 
 This metric only works for `StateSpaceSet`s whose elements are `SVector`s.
+
+For developers: `set_distance` can take keywords `tree1, tree2` that are the KDTrees
+of the first and second sets respectively.
 """
 struct Hausdorff{M<:Metric}
     metric::M
@@ -79,12 +82,15 @@ The distance is calculated with the given metric.
 
 The `brute::Bool` argument switches the computation between a KDTree-based version,
 or brute force (i.e., calculation of all distances and picking the smallest one).
-Brute force performs better for datasets that are either large dimensional or
+Brute force performs better for sets that are either large dimensional or
 have a small amount of points. Deciding a cutting point is not trivial,
 and is recommended to simply benchmark the [`set_distance`](@ref) function to
 make a decision.
 
 If `brute = false` this metric only works for `StateSpaceSet`s whose elements are `SVector`s.
+
+For developers: `set_distance` can take a keyword `tree2` that is the KDTree
+of the second set.
 """
 struct StrictlyMinimumDistance{M<:Metric}
     brute::Bool
